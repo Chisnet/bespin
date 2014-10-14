acab4cc061413ebe3318cd98578923f343e0a6ca
Chris Graham <chisnet@hotmail.com>
2014-10-14 21:34:55 +0100

    Split templates into separate JS file

--- _site/js/core.js
+++ _site/js/core.js
@@ -1,4 +1,4 @@
-// Bespin
+// Bespin Core
 var bespin = window.bespin || {};
 $.extend(bespin, {
 	status: 'disconnected',
@@ -16,48 +16,6 @@ $.extend(bespin, {
 		total: 0
 	},
 	unassigned_nodes: false,
-	templates: {
-		alias_view: {
-			alias: _.template(
-				'<div class="alias">\
-					<div class="name"><%= name %></div>\
-					<div class="inner">\
-					<% _.each(indices, function(index) { %>\
-						<%= indexTemplate(index) %>\
-					<% }); %>\
-					</div>\
-				</div>'
-			),
-			index: _.template(
-				'<div class="index index-<%= index.name %>" data-name="<%= index.name %>">\
-					<div class="name"><%= index.name %></div>\
-					<div class="info docs">Documents: <%= index.docs %></div>\
-					<div class="info size">Size: <%= index.size %></div>\
-				</div>'
-			)
-		},
-		table_view: {
-			table: _.template(
-				'<table cellpadding="0" cellspacing="0">\
-					<thead></thead>\
-					<tbody></tbody>\
-				</table>'
-			),
-			node: _.template(
-				'<th class="node">\
-					<span class="name"><%= name %></span><br/>\
-					<span class="hostname"><%= hostname %></span>\
-				</th>'
-			),
-			index: _.template(
-				'<th class="index">\
-					<span class="name"><%= name %></span><br/>\
-					<span class="info docs">Documents: <%= docs %></span><br/>\
-					<span class="info size">Size: <%= size %></span>\
-				</th>'
-			)
-		}
-	},
 
 	init: function(){
 		// Get the view type first so when we connect we can render immediately in the correct view
