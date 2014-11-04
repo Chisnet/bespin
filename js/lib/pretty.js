4b07cf20929d26290c35c4e4f9f914987ba4ab2e
Chris <chisnet@hotmail.com>
2014-11-04 14:15:56 +0000

    Added improved version of pretty which escapes HTML in strings

--- js/lib/pretty.js
+++ js/lib/pretty.js
@@ -1,4 +1,23 @@
+/* Pretty Code v0.11 */
 (function(window){
+    var escape_html = function(string) {
+        var escaped_string = '';
+        // Create map of replacements
+        var html_replacements = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#x27;',
+            '`': '&#x60;'
+        };
+        // Find and replace any occurences
+        escaped_string = string.replace(/&|<|>|"|'|`/g, function(match) {
+            return html_replacements[match];
+        });
+
+        return escaped_string;
+    };
     var pretty = {
         parse: function(data, data_type) {
             var func_name = 'parse_' + data_type;
@@ -6,14 +25,18 @@
                 return '<div class="pretty_output pretty_' + data_type + '">' + this[func_name](data) + '</div>';
             }
             catch(e) {
+                // If anything can't be processed for whatever reason just return false
                 return false;
             }
         },
         parse_json: function(data) {
             var result = '';
+            // Find the type of JSON data we've been given
             var value_type = data == null ? 'null' : data.constructor.name.toLowerCase();
+            // Based on the type of the data process/format accordingly
             switch(value_type) {
                 case 'object':
+                    // If we're dealing with an object process it's children recursively
                     var children = '';
                     var key_num=0, key_count = Object.keys(data).length;
                     for(child in data) {
@@ -23,6 +46,7 @@
                     result = '{' + ((children != '') ? '<ul>' + children + '</ul>' : '') + '}';
                     break;
                 case 'array':
+                    // If we're dealing with an array process it's elements recursively 
                     var elements = '';
                     for(var i=0; i<data.length; i++) {
                         var element = data[i];
@@ -31,12 +55,15 @@
                     result = '[' + ((elements != '') ? '<ul>' + elements + '</ul>' : '') + ']';
                     break;
                 case 'null':
+                    // Null's are a special highlighting case
                     result = '<span class="null">null</span>';
                     break;
                 case 'string':
-                    result = '<span class="string">"' + data + '"</span>';
+                    // String need additional quotes to be valid JSON
+                    result = '<span class="string">"' + escape_html(data) + '"</span>';
                     break;
                 default:
+                    // Everything else can be simply displayed as is
                     result = '<span class="' + value_type + '">' + data + '</span>';
                     break;
             }
