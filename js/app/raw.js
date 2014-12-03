75c65a42bfef368f9a04a35497322c1044bd4a6a
Chris <chisnet@hotmail.com>
2014-12-03 13:56:34 +0000

    Bit more work on raw JSON view

--- js/app/raw.js
+++ js/app/raw.js
@@ -1,7 +1,8 @@
-define([], function() {
+define(['jquery'], function($) {
     var raw = {
         init: function() {
-            
+            // Set the initial text in the data area
+            $('#raw_data').text('{\n    "query": {\n        "match_all": {}\n    }\n}');
         }
     };
     raw.init();
